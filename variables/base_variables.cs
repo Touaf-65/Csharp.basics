0230b7a85590a0c1a964b5fa6bad1a1216ee19d0
Touafic BOUAB <eclairetoilegentlemans@gmail.com>
2024-02-21 17:06:34 -0500

    Add Explanations

--- variables/base_variables.cs
+++ variables/base_variables.cs
@@ -43,3 +43,41 @@ namespace Variables
     }
 }
 
+/*
+        ** Explication **
+    
+    Une variable est représentée par son nom, caractérisée 
+    par son type et contient une valeur.
+
+    L'exemple d'un montant, d'un taux de reduction, d'une valeur 
+    "oui ou non", d'un nom qui peuvent etre stockes.
+
+    Prenant l'exemple de la variable amount(montant), elle peut 
+    etre stockee sous forme d'un entier.
+
+                int amount;
+
+    On appelle cela  "Declaration de la variable amount".
+    int permet de preciser au compilateur que amount est un entier.
+
+    Ici, la variable  amount n’a pas été initialisée, elle ne pourra 
+    pas être utilisée car le compilateur ne sait pas quelle valeur il y 
+    a dans la variable amount.
+
+    Alors on l'initialise en lui affectant une valeur:
+
+                int amount;
+                amount = 30000;
+    Des lors, amount contient la valeur 30000 jusqu'a ce quelle ne subisse 
+    une mise a jour.
+
+
+
+    Cela dit, il est possible d'initialiser une variable au moment de la 
+    declaration de celle ci.
+
+                string firstName = "Cynthia";
+
+    On cree ici notre variable de type string( chaime de caracteres) qui a 
+    pour valeur initiale 'Cynthia' 
+*/
