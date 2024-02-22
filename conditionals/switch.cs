2ea439e5af82f2ee82641d8da64804eb02bdbbb8
Touafic BOUAB <eclairetoilegentlemans@gmail.com>
2024-02-21 23:37:57 -0500

    Adding explanations on Switch usage

--- conditionals/switch.cs
+++ conditionals/switch.cs
@@ -70,3 +70,35 @@ namespace SwitchConditional
         }
     }
 }
+
+/*
+        ** Explication **
+    
+    L’instruction switch peut être utilisée lorsqu’une variable peut 
+    prendre beaucoup de valeurs. Elle permet de simplifier
+    l’écriture.
+
+    ~ Des que la necessite se fait ressentir de faire plusieurs controles 
+    sur une variable et mener des actions differentes pour chaque valeur,
+    on peut utiliser Switch case.
+
+    Exemple: 1 et 2.
+    ~ Case ... :
+        ..
+        ..
+      break;
+
+      break permet de marquer la fin d'un bloc Case.
+
+    ~ Si pour une valeur differente de toutes les valeurs controlees,
+    on la specifie dans le bloc default.
+
+    ~ SI pour differentes valeurs de la variable de controle on doit mener 
+    la meme action comme dans le bloc de code 3, il suffira de lisser nos blocs 
+    Case a la suite l'un de l'autre avec les differentes valeurs.
+
+    Apres la derniere valeur ce controle, on ecrit l'action a executer pour
+    ces conditions communes.
+
+    Exemple: 3 et 4 
+*/
