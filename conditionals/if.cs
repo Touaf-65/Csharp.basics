3ff4c05cb640b806b20e2b770e0aa19daa0d20a7
Touafic BOUAB <eclairetoilegentlemans@gmail.com>
2024-02-21 19:07:17 -0500

    Adding explanations on if conditions

--- conditionals/if.cs
+++ conditionals/if.cs
@@ -12,7 +12,7 @@ namespace IfConditional
             //1
             string hasRight = 'OK';
             if (hasRight == 'OK')
-            Console.WriteLine("Voua avez les droits d'acces");
+            Console.WriteLine("Vous avez les droits d'acces");
 
             //2
             decimal compteEnBanque = 300;
@@ -51,3 +51,36 @@ namespace IfConditional
         }
     }
 }
+
+/*
+        ** Explication **
+
+    //1
+    Ce code vérifie la valeur de la variable hasRight. S'il est égal 
+    à 'OK', le message "Vous avez les droits d'accès" s'affiche.
+
+    //2 
+    Ce code vérifie la valeur de la variable compteEnBanque. S'il est 
+    supérieur ou égal à 0, le message « Votre compte est créditeur » 
+    s'affiche. Sinon, le message « Votre compte est débiteur » s'affiche.
+
+    //3
+    Ce code vérifie la valeur de la variable estVrai. Si c'est vrai, le 
+    message "C'est vrai !" (C'est vrai !) s'affiche. Sinon, le message 
+    "C'est faux !" (C'est faux !) s'affiche.
+
+    //4
+    Ce code utilise l'opérateur ! pour annuler la valeur de la variable estVrai.
+    Puisque estVrai est faux, l'opérateur ! le rend vrai, et le message "C'est vrai !" 
+    (C'est vrai !) s'affiche.
+
+    //5 
+    Ce code vérifie les valeurs des variables login et motDePasse. S'ils sont tous 
+    deux égaux respectivement à "Nicolas" et "test", le message "Bienvenue Nicolas"
+    s'affiche. Sinon, le message « Login incorrect » (Connexion incorrecte) s'affiche.
+
+    //6
+    Ce code vérifie la valeur de la variable civilité. S'il est égal à "Mme" ou "Mlle", 
+    le message "Vous êtes une femme" s'affiche. Sinon, le message « Vous êtes un homme »
+    s'affiche.
+*/
