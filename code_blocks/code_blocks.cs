f350d2a773b5f3c5e2eda413ef9a35c0eed978b2
Touafic BOUAB <eclairetoilegentlemans@gmail.com>
2024-02-22 00:07:45 -0500

    Adding Explanations on  code blocks

--- code_blocks/code_blocks.cs
+++ code_blocks/code_blocks.cs
@@ -8,13 +8,14 @@ namespace CodeBlocks
     class Program
     {
         static void Main(string[] args)
-        {
+        {   
+            //1
             decimal compteEnBanque = 300;
             if (compteEnBanque >= 0)
             Console.WriteLine("Votre compte est crediteur");
             Console.WriteLine("Voullez-vous effectuer une transaction?");
 
-
+            //2
             decimal compteEnBanque = 300;
             if (compteEnBanque >= 0)
             {
@@ -30,3 +31,32 @@ namespace CodeBlocks
         }
     }
 }
+
+/*
+        ** Explication **
+
+    Un bloc de code est un regroupement d'instructions a executer a la suite l'un de l'autre
+    dans le meme contexte.
+
+    //1 Absence d'accolades
+    Apres la verification de la condition if, l'instruction juste apres if sera executee 
+    conformement a la valeur de la condition.
+
+    Donc ici, puisque 300 >= 0; on aura:
+        Votre compte est créditeur
+
+    Puis independemment des valeurs de compteEnBanque(que compteEnBanque >= 0 ou compteEnBanque=<0), le systeme affichera en console:
+        Voullez-vous effectuer une transaction?
+
+    
+    //2 Utilisation d'acolades
+    Toutes les lignes d'instructions s'executent seulement dans le contexte de definition.
+
+    Ici, le systeme affiche ses deux lignes seulement si compteEnBanque >=0
+        Votre compte est crediteur
+        Voullez-vous effectuer une transaction?
+    
+    Quand compteEnBanque < 0 alors s'affiche le message dans le bloc else.
+        Vous ne pouvez qu'effectuer une operation  de transaction
+
+*/
